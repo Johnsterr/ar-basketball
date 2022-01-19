96a60275c2b73df8306a61ad2e5580a9a506c84f
Evgeniy Pashko <evgpashko92@gmail.com>
2022-01-19 21:41:28 +0700

    Complete Project

---
+++ AR Basketball/Models/BodyType.swift
@@ -0,0 +1,15 @@
+//
+//  BodyType.swift
+//  AR-Basketball
+//
+//  Created by Евгений Пашко
+//
+
+
+enum BodyType: Int{
+    case ball = 1
+    case board = 2
+    case rim = 4
+    case topPlane = 8
+    case bottomPlane = 16
+}
